ef220c06b34c45cbcf1a93bca7df3e38f3c46c5f
David Sobíšek <dsobisek04@gmail.com>
2026-04-07 10:50:39 +0200

    VM update

--- composeApp/src/commonMain/kotlin/cz/uuk/hek/domain/vm/DashboardVM.kt
+++ composeApp/src/commonMain/kotlin/cz/uuk/hek/domain/vm/DashboardVM.kt
@@ -1,22 +1,30 @@
 package cz.uuk.hek.domain.vm
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import cz.uuk.hek.presentation.interfaces.DashboardActions
 import cz.uuk.hek.presentation.uistates.DashboardUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class DashboardVM {
+class DashboardVM: ViewModel() {
 
     private val _uiState= MutableStateFlow(DashboardUiState())
     val uiState: StateFlow<DashboardUiState> = _uiState
+    //private val repository
+
+    init {
+        loadLessons()
+    }
 
     fun onDashboardAction(action: DashboardActions){
         when(action)
         {
             is DashboardActions.SetIsLoading -> setLoading(action.isLoading)
-            is DashboardActions.LoadLessons -> TODO()
-            is DashboardActions.OpenLesson -> TODO()
+            is DashboardActions.LoadLessons -> loadLessons()
+            is DashboardActions.OpenLesson -> setCurrentLesson(action.id)
         }
     }
     private fun setLoading(isLoading: Boolean)
@@ -25,7 +33,10 @@ class DashboardVM {
     }
     private fun loadLessons()
     {
+        /*viewModelScope.launch {
+            repository.lessons.collect(it->_uistate.update(lessons=it))
 
+        }*/
     }
 
     private fun setCurrentLesson(id: String)
